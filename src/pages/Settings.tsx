ff3c5eea9c2896c1334f865fb129d810337778d1
fannywolff <fanny.wolff@web.de>
2024-07-08 17:16:35 +0200

    settings, register css

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,23 +1,40 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar} from '@ionic/react';
 import React from "react";
 import Greeting from "../components/Greeting";
 import CalenderProgressBar from "../components/charts/CalenderProgressBar";
 import Calender from "../components/Calender";
 import AddStepsModal from "../components/modals/AddStepsModal";
+import {arrowBack, arrowForwardOutline, logOutOutline, personOutline, settingsOutline} from "ionicons/icons";
+import user from "./Tab4/User";
 
 const Settings: React.FC = () => {
     return (
         <IonPage className={"PageModal Edit settings"} >
             <IonContent fullscreen>
+                <h1>Einstellungen</h1>
                 <Greeting name={"wilder Esel"}/>
-                <div>
-                    <button onClick={() => console.log("Nutzer clicked")}>Nutzer-Einstellungen</button>
-                </div>
-                <div>
-                    <button onClick={() => console.log("App clicked")}>App-Einstellungen</button>
-                </div>
-                <div>
-                    <button onClick={() => console.log("Logout clicked")}>Logout</button>
+                <div className={"settingsContainer"}>
+                    <div className={"flexSetting"}>
+                        <div>
+                            <IonIcon aria-hidden="true" icon={personOutline}/>
+                            <button onClick={() => console.log("Nutzer clicked")}>Nutzer-Einstellungen</button>
+                        </div>
+                        <IonIcon aria-hidden="true" icon={arrowForwardOutline}/>
+                    </div>
+                    <div className={"flexSetting app"}>
+                        <div>
+                            <IonIcon aria-hidden="true" icon={settingsOutline}/>
+                            <button onClick={() => console.log("App clicked")}>App-Einstellungen</button>
+                        </div>
+                        <IonIcon aria-hidden="true" icon={arrowForwardOutline}/>
+                    </div>
+                    <div className={"flexSetting logout"}>
+                        <div>
+                            <IonIcon aria-hidden="true" icon={logOutOutline}/>
+                            <button onClick={() => console.log("Logout clicked")}>Logout</button>
+                        </div>
+                        <IonIcon aria-hidden="true" icon={arrowForwardOutline}/>
+                    </div>
                 </div>
 
 
