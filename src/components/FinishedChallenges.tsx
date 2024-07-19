cad87e7317d8682805d995f5922bd549166db913
einToast <laurin2102@gmail.com>
2024-07-19 11:44:08 +0200

    fix FinishedChallenges

--- src/components/FinishedChallenges.tsx
+++ src/components/FinishedChallenges.tsx
@@ -21,7 +21,7 @@ const FinishedChallenges: React.FC<{finishedChallenges: UserChallengeDTO[]}> = (
         challengeList.push(
             <div className={"challenge"} key={0}>
                 <div>
-                    <p>{"Sieht so aus als hättest noch keine Herausforderungen absolviert"}</p>
+                    <p>{"Es sieht so aus als hättest du noch keine Herausforderungen absolviert"}</p>
                     <p className={"counter"}>{"Schau doch mal bei den aktuellen Herausforderungen vorbei!"}</p>
                 </div>
                 <p className={"astatus"}>{"Aktiv"}</p>
